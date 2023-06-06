2a83f590230992d5db04a4307aaff4746e0b17b9
Cipher Phantom <oyepitan10@gmail.com>
2023-06-06 23:14:11 +0100

    Fix Errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 for i in range(10):
     for j in range(10):
         if i  == 0:
-            print(f"{i}{j}", end=", ")
+            print("{}{}".format(i, j), end=", ")
         elif i < j:
             if i * 10 + j < 89:
-                print(f"{i}{j}", end=", ")
+                print("{}{}".format(i, j), end=", ")
             else:
-                print(f"{i}{j}")
+                print("{}{}".format(i, j))
