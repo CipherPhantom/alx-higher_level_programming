f362bd78d01b792f3e9abe90e01136265d563cf7
Cipher Phantom <oyepitan10@gmail.com>
2023-06-07 01:13:04 +0100

    Task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include "stdlib.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ *
+ * @head: Pointer to head node pointer.
+ * @number: New number.
+ *
+ * Return: Address of the new node, or Null if it failed.
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *current, *previous;
+
+	if (!head)
+		return (NULL);
+
+	current = *head;
+	previous = *head;
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+	node->n = number;
+	node->next = NULL;
+
+	while (current)
+	{
+		if (current->n > number)
+		{
+			previous->next = node;
+			node->next = current;
+			break;
+		}
+		previous = current;
+		current = current->next;
+	}
+	if (!current)
+		previous->next = node;
+
+	return (node);
+}
