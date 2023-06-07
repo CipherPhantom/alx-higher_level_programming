d164cb4994fe225593595391791291278be908c1
Cipher Phantom <oyepitan10@gmail.com>
2023-06-07 07:05:16 +0100

    Error Fix 12

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -26,7 +26,7 @@ listint_t *insert_node(listint_t **head, int number)
 	node->next = NULL;
 
 	if (!*head)
-		*head = node;
+		**head = &node;
 	while (current)
 	{
 		if (current->n > number && !previous)
