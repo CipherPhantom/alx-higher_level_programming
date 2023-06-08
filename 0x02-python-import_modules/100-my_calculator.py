4d9d73a5dce17f22e5ce2e1404654cf1457d7dec
Cipher Phantom <oyepitan10@gmail.com>
2023-06-08 20:53:15 +0100

    Error Fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     if argv_len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     operator = sys.argv[2]
     if operator not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     if operator == "+":
