4979f41837b0542c532acdee86b46d89e1c86e75
Cipher Phantom <oyepitan10@gmail.com>
2023-06-08 20:44:11 +0100

    Advanced Tasks

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    argv_len = len(sys.argv) - 1
+
+    if argv_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    
+    operator = sys.argv[2]
+    if operator not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    else:
+        result = div(a, b)
+
+    print("{0} {2} {1} = {3}".format(a, b, operator, result))
+    sys.exit(0)
