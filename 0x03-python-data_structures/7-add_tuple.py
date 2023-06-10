a8e67d6136070952f665b90fa673ebc41b635dfa
Cipher Phantom <oyepitan10@gmail.com>
2023-06-10 16:07:09 +0100

    Error Sha

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def check_tuple(tuple_1):
-
     if len(tuple_1) == 0:
         return (0, 0)
     elif len(tuple_1) == 1:
@@ -11,11 +9,8 @@ def check_tuple(tuple_1):
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-
     tuple_a = check_tuple(tuple_a)
     tuple_b = check_tuple(tuple_b)
-
     one = tuple_a[0] + tuple_b[0]
     two = tuple_a[1] + tuple_b[1]
-
     return (one, two)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-
     if sentence == "":
         return (len(sentence), None)
     else:
