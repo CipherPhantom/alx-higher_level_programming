f9ea7e56f70f91f05e0ec038e65ac9207233b6a6
Cipher Phantom <oyepitan10@gmail.com>
2023-06-10 15:37:35 +0100

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def check_tuple(tuple_1):
+
+    if len(tuple_1) == 0:
+        return (0, 0)
+    elif len(tuple_1) == 1:
+        return (tuple_1[0], 0)
+    else:
+        return tuple_1
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    tuple_a = check_tuple(tuple_a)
+    tuple_b = check_tuple(tuple_b)
+
+    one = tuple_a[0] + tuple_b[0]
+    two = tuple_a[1] + tuple_b[1]
+
+    return (one, two)
