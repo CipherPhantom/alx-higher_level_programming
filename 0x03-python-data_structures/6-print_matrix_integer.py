3814677d8d57f57cc7773c52419f504bdf955ac2
Cipher Phantom <oyepitan10@gmail.com>
2023-06-10 16:26:07 +0100

    Error Fix 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
+    for row in matrix:
+        for element in row:
+            if row[-1] == element:
+                print("{:d}".format(element), end="")
             else:
-                print("{:d}".format(matrix[i][j]), end=" ")
+                print("{:d}".format(element), end=" ")
+            print()
