4bc0c0af40fe7548e66dffcc7b03b6f53c53f07d
Cipher Phantom <oyepitan10@gmail.com>
2023-06-11 18:06:03 +0100

    Task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a listint_t linked list.
+ * @head: Pointer to the head pointer if the listint_t list
+ *
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next;
+
+	if (!head)
+		return (NULL);
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *reversed, *current;;
+	int isPalindrome = 1;
+
+	if (!head || !*head)
+		return (isPalindrome);
+
+	current = *head;
+	reversed = reverse_listint(head);
+
+	while(current && reversed)
+	{
+		if(current->n != reversed->n)
+		{
+			isPalindrome = 0;
+			break;
+		}
+		current = current->next;
+		reversed = reversed->next;
+	}
+	return (isPalindrome);
+}
