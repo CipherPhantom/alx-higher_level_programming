6c307eb015423912181bc0eb086de67bc8317866
Cipher Phantom <oyepitan10@gmail.com>
2023-06-26 21:30:50 +0100

    Use Exception Keyword Instead

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,10 +3,9 @@ import sys
 
 
 def safe_function(fct, *args):
-    errors = (ValueError, TypeError, ZeroDivisionError, IndexError)
     try:
         result = fct(args[0], args[1])
-    except errors as err:
+    except Exception as err:
         result = None
         print("Exception: {}".format(err), file=sys.stderr)
     return result
