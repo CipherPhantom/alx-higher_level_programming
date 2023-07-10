6a7c73530f57157e7280b7e14a33152ec0ddf462
Cipher Phantom <oyepitan10@gmail.com>
2023-07-10 23:53:38 +0100

    Mandatory Task - Fix Error_2

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,8 +10,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates if input is an integer
-
+        """Validates if value is an integer greater than 0
         Args:
             name (str): The name of the paremeter
             value (int): The value of the parameter
@@ -20,7 +19,7 @@ class BaseGeometry:
             TypeError: If `value` is not integer
             ValueError: If `value` is less than or equal to zero
         """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
