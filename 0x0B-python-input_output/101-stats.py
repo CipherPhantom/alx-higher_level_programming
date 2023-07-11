ab0b79214ee65170956edbfc4b833613c074c7f8
Cipher Phantom <oyepitan10@gmail.com>
2023-07-11 23:56:03 +0100

    Fix 101-stats.py ..1

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 """A script that reads stdin line by line and computes metrics."""
-import sys, time
+
+if __name__ == "__main__":
+    import sys, time
 
 
-file_size = 0
-stats = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-}
+    file_size = 0
+    stats = {
+            "200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0
+    }
 
-try:
-    count = 0
-    for line in sys.stdin:
-        count += 1
-        info = line.split()
-        file_size += int(info[-1])
-        stats[info[-2]] += 1
+    try:
+        count = 0
+        for line in sys.stdin:
+            count += 1
+            info = line.split()
+            file_size += int(info[-1])
+            stats[info[-2]] += 1
 
-        if count % 10 == 0:
-            stats_text = ""
-            stats_text += f"File size: {file_size}\n"
-            for key, value in stats.items():
-                if value > 0:
-                    stats_text += f"{key}: {value}\n"
-            print(stats_text, end="")
+            if count % 10 == 0:
+                stats_text = ""
+                stats_text += f"File size: {file_size}\n"
+                for key, value in stats.items():
+                    if value > 0:
+                        stats_text += f"{key}: {value}\n"
+                print(stats_text, end="")
 
-except KeyboardInterrupt as e:
-    stats_text = ""
-    stats_text += f"File size: {file_size}\n"
-    for key, value in stats.items():
-        if value > 0:
-            stats_text += f"{key}: {value}\n"
-    print(stats_text, end="")
-    time.sleep(1)
+    except KeyboardInterrupt as e:
+        stats_text = ""
+        stats_text += f"File size: {file_size}\n"
+        for key, value in stats.items():
+            if value > 0:
+                stats_text += f"{key}: {value}\n"
+        print(stats_text, end="")
+        raise
