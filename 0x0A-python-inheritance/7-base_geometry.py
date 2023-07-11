0fb3315c1733d89d71cf398ed2f4458a63679741
Cipher Phantom <oyepitan10@gmail.com>
2023-07-11 08:45:48 +0100

    Fix 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
         """Not yet implemented"""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name, value=None):
         """Validates if value is an integer greater than 0
         Args:
             name (str): The name of the paremeter
