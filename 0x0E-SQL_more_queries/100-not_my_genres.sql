e19984aab1c9b14eb7ea9921eaedf296681d3b96
Cipher Phantom <oyepitan10@gmail.com>
2023-08-17 03:19:21 +0100

    Complete Advanced Task
    Fix errors in mandatory task

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,15 @@
+-- This script uses the hbtn_0d_tvshows database to list
+-- all genres not linked to the show Dexter
+
+SELECT name
+FROM tv_genres
+WHERE name NOT IN (
+	SELECT tv_genres.name
+	FROM tv_show_genres
+	JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = "Dexter"
+)
+ORDER BY name;
