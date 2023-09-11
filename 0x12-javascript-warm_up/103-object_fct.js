31d17d7cc8ec363657594ed6e3fb6b12c3d3c5b0
Cipher Phantom <oyepitan10@gmail.com>
2023-09-11 21:45:54 +0100

    Advanced Tasks

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// This script adds a new function incr that increments the integer value
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value += 1;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
