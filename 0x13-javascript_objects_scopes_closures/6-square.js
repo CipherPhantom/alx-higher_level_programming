bd028afe47128fe395fe0ea1ed471bd3fe01f088
Cipher Phantom <oyepitan10@gmail.com>
2023-09-12 17:32:08 +0100

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// This script creates a class Square that defines a square and inherits from Rectangle
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let breath = '';
+        for (let j = 0; j < this.width; j++) {
+          breath += c;
+        }
+        console.log(breath);
+      }
+    }
+  }
+}
+
+module.exports = Square;
