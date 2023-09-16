947aa23d3be9313b84d79a64fbbe9e17b42f882b
Cipher Phantom <oyepitan10@gmail.com>
2023-09-16 21:08:01 +0100

    Fix Error: 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     for state in query.all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
 
     session.close()
