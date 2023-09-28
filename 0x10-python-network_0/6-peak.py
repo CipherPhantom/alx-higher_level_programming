e9e7e9a978d3183b1f6e63baedef90eab089c8c8
Cipher Phantom <oyepitan10@gmail.com>
2023-09-28 15:33:04 +0100

    6-peak_1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,11 +8,4 @@ def find_peak(list_of_integers):
     n = len(list_of_integers)
     if n == 0:
         return None
-    if n == 1:
-        return list_of_integers[0]
-    left = list_of_integers[:n//2]
-    right = list_of_integers[n//2:]
-    if find_peak(left) > find_peak(right):
-        return find_peak(left)
-    else:
-        return find_peak(right)
+    return max(list_of_integers)
